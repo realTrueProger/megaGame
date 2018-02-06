f56f034dc639a479196f12012f90576326ff598e
trueProger <vovasollo@yandex.ru>
2018-02-06 16:09:30 +0300

    Логика игры готова

--- js/main.js
+++ js/main.js
@@ -1,24 +1,104 @@
-// Заполнение игрового поля картинками
 
-function randomPics() {
-    let pictureClasses = ['img1', 'img2', 'img3', 'img4', 'img5', 'img6', 'img7', 'img8'];
-    let tds = document.querySelectorAll('td');
-    tds = Array.prototype.slice.call(tds);
-    for(let i = 0; i < pictureClasses.length; i++) {
-        for (let j = 0; j < 2; j++) {
-            let random = Math.floor(Math.random() * tds.length);
-            tds[random].classList.add(pictureClasses[i]);
-            tds.splice(random, 1);
+// начало игры
+
+window.onload = () => {
+    coverWithBackImg();
+    let startBtn = document.getElementById('startBtn');
+    let toCheck = [];
+    let p = document.getElementById('instructions');
+    let guesses = 0;
+
+    startBtn.addEventListener('click', startGame);
+
+    // Старт игры по кнопке
+
+    function startGame(e) {
+        startBtn.style.display = 'none';
+        p.innerHTML = 'Вам необходимо запомнить расположение картинок. Поторопитесь!';
+        randomPics();
+        setTimeout( () => {
+            p.innerHTML = 'Теперь пора вспомнить увиденное, раскрывайте карты парами';
+            coverWithBackImg();
+            gameProcessInit();
+        }, 3000);
+    }
+
+    // Инициализация обработчиков картинок
+
+    function gameProcessInit() {
+        let tds = document.querySelectorAll('td');
+        for (let i = 0; i < tds.length; i++) {
+            tds[i].addEventListener('click', sendToCheck);
         }
     }
-}
 
-function coverWithBackImg () {
-    let tds = document.querySelectorAll('td');
-    for (let i = 0; i < tds.length; i++) {
-        tds[i].classList.add('backImg');
+    // Отправляем картинки на проверку
+
+    function sendToCheck(e) {
+        e.target.classList.remove('backImg');
+        if(toCheck.indexOf(e.target) === -1) {
+            toCheck.push(e.target);
+            if (toCheck.length > 1) {
+                check(toCheck);
+                toCheck = [];
+            }
+        }
+        
     }
-}
 
-//randomPics();
-coverWithBackImg();
+    // проверка картинок и победа
+
+    function check(arr) {
+        if (arr[0].classList.value === arr[1].classList.value) {
+            setTimeout( () => {
+                p.innerHTML = 'Ура! Верно';
+                arr.forEach( (item) => {
+                    item.style.visibility = 'hidden';
+                });
+                guesses++;
+                console.log(guesses);
+
+                // победа в игре
+
+                if (guesses === 8) {
+                    p.innerHTML = 'Это победа. Вы победитель и в игре и по жизни! Мои поздравления.';
+                }
+            }, 500);
+
+        } else {
+            setTimeout( () => {
+                p.innerHTML = 'Увы..неверная пара';
+                arr.forEach( (item) => {
+                    item.classList.toggle('backImg');
+                });
+            }, 500);
+
+        }
+    }
+
+    // Заполнение игрового поля картинками
+
+    function randomPics() {
+        let pictureClasses = ['img1', 'img2', 'img3', 'img4', 'img5', 'img6', 'img7', 'img8'];
+        let tds = document.querySelectorAll('td');
+        tds = Array.prototype.slice.call(tds);
+        for(let i = 0; i < pictureClasses.length; i++) {
+            for (let j = 0; j < 2; j++) {
+                let random = Math.floor(Math.random() * tds.length);
+                tds[random].classList.add(pictureClasses[i]);
+                tds[random].classList.toggle('backImg');
+                tds.splice(random, 1);
+            }
+        }
+    }
+
+    // Рубашка для карт
+
+    function coverWithBackImg () {
+        let tds = document.querySelectorAll('td');
+        for (let i = 0; i < tds.length; i++) {
+            tds[i].classList.add('backImg');
+        }
+    }
+
+};
